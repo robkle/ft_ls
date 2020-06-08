bf916e773f2727c43a6c69f83e2f0e5094238135
Robert Klein <rklein@c3r2p7.hive.fi>
2020-06-08 14:39:41 +0300

    usage bug fixed

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rklein <rklein@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/05 12:22:42 by rklein            #+#    #+#             */
-/*   Updated: 2020/06/05 12:23:58 by rklein           ###   ########.fr       */
+/*   Updated: 2020/06/08 14:38:25 by rklein           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	main(int argc, char **argv)
 	i = 1;
 	if (argc > 1)
 		i = ft_flags(argc, argv, flags);
+	if (i == -1)
+		return (0);
 	ft_ls(argc, argv, i - 1, flags);
 	free(flags);
 	return (0);
